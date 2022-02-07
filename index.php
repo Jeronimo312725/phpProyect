c2c907f12a93e1f282f61b0aca8a819a5100cf31
Jeronimo312725 <marinjeronimo44@gmail.com>
2022-02-07 16:22:20 -0500

    se agrega estructura de carpetas php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+include_once('php/header.php')
+
+?>
+
+<body>
+   <div class="container"> 
+       jeronimo
+   </div>
+</body>
+<?php 
+include_once('php/footer.php')
+
+?>
---
+++ php/CrearUsuario.controller.php
@@ -0,0 +1,15 @@
+<?php 
+$nombres=@$_POST['nombres'];
+$apellidos=@$_POST['apellidos'];
+$apodo=@$_POST['apodo'];
+$ciudad=@$_POST['ciudad'];
+$telefono=@$_POST['telefono'];
+
+
+echo $nombres ."<br> <br>"; 
+echo $apellidos. "<br> <br>";  
+echo $apodo ."<br> <br>";  
+echo $ciudad. "<br> <br>";  
+echo $telefono ;
+
+?>
