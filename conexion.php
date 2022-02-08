7d90e04c058a99ea726c6e43e62082c1652db2a1
AlejandroV2204 <jvallarinocruz@gmail.com>
2022-02-08 15:12:16 -0500

    Se realizó el controlador

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-/*$servername = "localhost";
-$database = "pruebaphp";
-$username = "root";
-$password = "";
+/*$servername = "db4free.net";
+$database = "sgmudev";
+$username = "udev_edu";
+$password = "OscarJavier";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
--- reporte/crearReporte.controller.php
+++ reporte/crearReporte.controller.php
@@ -1,16 +1,18 @@
 <?php 
 
+include_once('../header.php');
 include_once('../conexion.php');
+
 
 $id_reporte = @$_POST['id_reporte'];
 $id_usuario1 = @$_POST['id_usuario1'];
 $id_PC1 = @$_POST['id_PC1'];
 $descripcion = @$_POST['descripcion'];
-$password = "password";
 
-echo "<br>" . $id_reporte . "<br>" . $id_usuario1 . "<br>" . $id_PC1 . "<br>" . $descripcion .  "<br><br>" ;
 
-/*$sql = "INSERT INTO usuario VALUES ('default', '$id_reporte', '$id_usuario1', '$id_PC1', '$descripcion')";
+/*echo "<br>" . $id_reporte . "<br>" . $id_usuario1 . "<br>" . $id_PC1 . "<br>" . $descripcion .  "<br><br>" ;
+
+$sql = "INSERT INTO usuario VALUES ('default', '$id_usuario1', '$id_PC1', '$descripcion')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
@@ -20,6 +22,39 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);*/
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
+<table class="table table-dark table-striped">
 
+    <tr>
+        <th>ID_Reporte</th>
+        <th>ID_Usuario</th>
+        <th>ID_PC</th>
+        <th>Descripción</th>
+    </tr>
+
+    <tr>
+        <td> <?php echo $id_reporte; ?> </td>
+        <td> <?php echo $id_usuario1; ?> </td>
+        <td> <?php echo $id_PC1; ?> </td>
+        <td> <?php echo $descripcion; ?> </td>
+    </tr>
+  
+
+</table>
+
+</body>
+</html>
+
+<?php 
+include_once('../footer.php');
 ?>
+
