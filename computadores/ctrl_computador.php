d1ea5fa2d035eb06a2c01ce4140a27c44e1d91f8
d1lanX <rodriguezdylan58@gmail.com>
2022-02-10 15:47:12 -0500

    controlador en computador se re-hace con prepared statement

--- computadores/ctrl_computador.php
+++ computadores/ctrl_computador.php
@@ -1,20 +1,39 @@
 <?php 
 
-    include_once('conexion.php');
-    include_once('header.php');
+    include_once('../conexion.php');
+    include_once('../header.php');
 
 
 
 
 
-    $conn = mysqli_connect("db4free.net", "udev_edu", "OscarJavier", "sgmudev");
+    // $conn = mysqli_connect("db4free.net", "udev_edu", "OscarJavier", "sgmudev");
 
-    if($conn === false){
+    // if($conn === false){
 
-        die("Error: no se pudo conectar. "
-                .mysqli_connect_error());
+    //     die("Error: no se pudo conectar. "
+    //             .mysqli_connect_error());
 
-    }
+    // }
+
+    
+
+
+
+    
+    $statement = $conn->prepare("insert into computador (id_computador, id_sala1, sistema_operativo, motherboard,
+    ram, velocidad_ram, procesador, tipo_graficos, capacidad_disco, mouse, teclado, estado_panel_frontal, lectora_cd, ventiladores,
+    cambio_pasta_termica, ultimo_mantenimiento, salidas_video) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+
+    $statement->bind_param("iisssssssssssssss", $idcomputador, $idsala, $sistema_operativo, $motherboard, $ram, $velocidadram, $procesador, $tipograficos,
+    $capacidaddiscoduro, $mouse, $teclado, $panelfrontal, $lectoradvd, $ventiladores, $pastatermica, $ultimomantenimiento, $salidavideo);
+
+
+    // $sql = "INSERT INTO usuario VALUES ('$idcomputador', '$idsala', '$sistema_operativo', '$motherboard', '$ram', 
+    // '$velocidadram', '$procesador', '$tipograficos', '$capacidaddiscoduro', '$mouse', '$teclado', '$panelfrontal',
+    // '$lectoradvd', '$ventiladores', '$pastatermica', '$ultimomantenimiento', '$salidavideo')";
+
 
     $idcomputador = $_REQUEST['id_pc'];
     $sistema_operativo = $_REQUEST['so_pc'];
@@ -35,23 +54,28 @@
     $salidavideo = $_REQUEST['salidas_video'];
 
 
-
-    $sql = "INSERT INTO usuario VALUES ('$idcomputador', '$idsala', '$sistema_operativo', '$motherboard', '$ram', 
-    '$velocidadram', '$procesador', '$tipograficos', '$capacidaddiscoduro', '$mouse', '$teclado', '$panelfrontal',
-    '$lectoradvd', '$ventiladores', '$pastatermica', '$ultimomantenimiento', '$salidavideo')";
-
-    if(mysql_query($conn, $sql)){
-        echo "<h3>Datos guardados</h3>";
-
-    } else{
-
-        echo "error".mysql_error($conn);
-    }
-
-    mysqli_close($conn);
+    $statement->execute();
 
 
+    echo "Datos guardados";
 
-    include_once('footer.php');
+
+    $statement->close();
+    $conn->close();
+
+
+    // if(mysql_query($conn, $sql)){
+    //     echo "<h3>Datos guardados</h3>";
+
+    // } else{
+
+    //     echo "error".mysql_error($conn);
+    // }
+
+    // mysqli_close($conn);
+
+
+
+    include_once('../footer.php');
 
 ?>
